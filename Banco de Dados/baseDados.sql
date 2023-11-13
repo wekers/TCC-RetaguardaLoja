1eeb2603ffec93e07c821edc83e191b6e67f36f2
Fernando Gilli <fernando@wekers.org>
2023-11-12 21:07:23 -0300

    remove COLLATE

--- Banco de Dados/baseDados.sql
+++ Banco de Dados/baseDados.sql
@@ -38,8 +38,8 @@ CREATE DATABASE "TccRetaguarda"
 CREATE TABLE IF NOT EXISTS usuarios
 (
     cod integer NOT NULL,
-    nome character varying(255) COLLATE ,
-    senha text COLLATE ,
+    nome character varying(255) ,
+    senha text ,
     CONSTRAINT usuarios_pkey PRIMARY KEY (cod)
 )
 
@@ -67,33 +67,33 @@ INSERT INTO usuarios(cod, nome, senha)
 CREATE TABLE IF NOT EXISTS clientes
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    nome character varying(255) COLLATE  NOT NULL,
-    apelido_ml character varying(255) COLLATE ,
-    fone character varying(255) COLLATE ,
-    email character varying(255) COLLATE ,
-    cep character varying(10) COLLATE ,
-    n_endereco character varying(255) COLLATE ,
-    logradouro character varying(255) COLLATE ,
-    bairro character varying(255) COLLATE ,
-    cidade character varying(255) COLLATE ,
-    uf character varying(5) COLLATE ,
-    email_alt character varying(255) COLLATE ,
-    celular character varying(10) COLLATE ,
-    cnpj_cpf character varying(14) COLLATE ,
-    cmun character varying(7) COLLATE ,
-    ie character varying(14) COLLATE ,
-    isuf character varying(9) COLLATE ,
-    logradouro_cpl character varying(60) COLLATE ,
-    tipo_c character varying(2) COLLATE ,
-    endereco_ent_dif character varying(2) COLLATE ,
-    d_nome character varying(100) COLLATE ,
-    d_cep character varying(10) COLLATE ,
-    d_n_endereco character varying(60) COLLATE ,
-    d_logradouro character varying(100) COLLATE ,
-    d_bairro character varying(100) COLLATE ,
-    d_cidade character varying(100) COLLATE ,
-    d_uf character varying(2) COLLATE ,
-    d_logradouro_cpl character varying(60) COLLATE ,
+    nome character varying(255)  NOT NULL,
+    apelido_ml character varying(255) ,
+    fone character varying(255) ,
+    email character varying(255) ,
+    cep character varying(10) ,
+    n_endereco character varying(255) ,
+    logradouro character varying(255) ,
+    bairro character varying(255) ,
+    cidade character varying(255) ,
+    uf character varying(5) ,
+    email_alt character varying(255) ,
+    celular character varying(10) ,
+    cnpj_cpf character varying(14) ,
+    cmun character varying(7) ,
+    ie character varying(14) ,
+    isuf character varying(9) ,
+    logradouro_cpl character varying(60) ,
+    tipo_c character varying(2) ,
+    endereco_ent_dif character varying(2) ,
+    d_nome character varying(100) ,
+    d_cep character varying(10) ,
+    d_n_endereco character varying(60) ,
+    d_logradouro character varying(100) ,
+    d_bairro character varying(100) ,
+    d_cidade character varying(100) ,
+    d_uf character varying(2) ,
+    d_logradouro_cpl character varying(60) ,
     CONSTRAINT clientes_pkey PRIMARY KEY (id)
 )
 
@@ -110,16 +110,16 @@ ALTER TABLE IF EXISTS clientes
 CREATE TABLE IF NOT EXISTS fornecedor
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    nome character varying(255) COLLATE ,
-    cep character varying(10) COLLATE ,
-    fone character varying(15) COLLATE ,
-    fax character varying(15) COLLATE ,
-    cnpj_cpf character varying(15) COLLATE ,
-    inscr_estadual character varying(15) COLLATE ,
-    email character varying(255) COLLATE ,
-    contato character varying(255) COLLATE ,
-    n_endereco character varying(255) COLLATE ,
-    tipo_c character varying(2) COLLATE ,
+    nome character varying(255) ,
+    cep character varying(10) ,
+    fone character varying(15) ,
+    fax character varying(15) ,
+    cnpj_cpf character varying(15) ,
+    inscr_estadual character varying(15) ,
+    email character varying(255) ,
+    contato character varying(255) ,
+    n_endereco character varying(255) ,
+    tipo_c character varying(2) ,
     CONSTRAINT fornecedor_pkey PRIMARY KEY (id)
 )
 
@@ -137,9 +137,9 @@ ALTER TABLE IF EXISTS fornecedor
 CREATE TABLE IF NOT EXISTS bairro
 (
     id_bairro integer NOT NULL,
-    id_uf character varying(2) COLLATE ,
+    id_uf character varying(2) ,
     id_cidade integer,
-    bairro_nome character varying(72) COLLATE 
+    bairro_nome character varying(72) 
 )
 
 TABLESPACE pg_default;
@@ -156,19 +156,19 @@ ALTER TABLE IF EXISTS bairro
 
 CREATE TABLE IF NOT EXISTS estado
 (
-    id_uf character varying(2) COLLATE  NOT NULL,
-    estado_nome character varying(72) COLLATE ,
-    faixa_cep1_ini character varying(8) COLLATE ,
-    faixa_cep1_fim character varying(8) COLLATE ,
-    faixa_cep2_ini character varying(8) COLLATE ,
-    faixa_cep2_fim character varying(8) COLLATE ,
+    id_uf character varying(2)  NOT NULL,
+    estado_nome character varying(72) ,
+    faixa_cep1_ini character varying(8) ,
+    faixa_cep1_fim character varying(8) ,
+    faixa_cep2_ini character varying(8) ,
+    faixa_cep2_fim character varying(8) ,
     pac numeric(10,2),
     sedex numeric(10,2),
-    capital character varying(255) COLLATE ,
-    prazo_capital_pac character varying(255) COLLATE ,
-    prazo_capital_sedex character varying(255) COLLATE ,
-    prazo_noc_pac character varying(255) COLLATE ,
-    prazo_noc_sedex character varying(255) COLLATE 
+    capital character varying(255) ,
+    prazo_capital_pac character varying(255) ,
+    prazo_capital_sedex character varying(255) ,
+    prazo_noc_pac character varying(255) ,
+    prazo_noc_sedex character varying(255) 
 )
 
 TABLESPACE pg_default;
@@ -185,10 +185,10 @@ ALTER TABLE IF EXISTS estado
 CREATE TABLE IF NOT EXISTS cidade
 (
     id_cidade integer NOT NULL,
-    id_uf character varying(2) COLLATE ,
-    cidade_nome character varying(60) COLLATE ,
-    cep character varying(9) COLLATE ,
-    codigo_municipio character varying(7) COLLATE 
+    id_uf character varying(2) ,
+    cidade_nome character varying(60) ,
+    cep character varying(9) ,
+    codigo_municipio character varying(7) 
 )
 
 TABLESPACE pg_default;
@@ -207,8 +207,8 @@ CREATE TABLE IF NOT EXISTS cidade_ibge
     id integer NOT NULL,
     id_estado integer,
     codigo_municipio integer,
-    nome character varying(250) COLLATE ,
-    uf character varying(2) COLLATE 
+    nome character varying(250) ,
+    uf character varying(2) 
 )
 
 TABLESPACE pg_default;
@@ -225,12 +225,12 @@ ALTER TABLE IF EXISTS cidade_ibge
 CREATE TABLE IF NOT EXISTS endereco
 (
     id_endereco integer NOT NULL,
-    id_uf character varying(2) COLLATE ,
+    id_uf character varying(2) ,
     id_cidade integer,
     id_bairro integer,
-    endereco_nome character varying(72) COLLATE ,
-    cep character varying(8) COLLATE ,
-    complemento character varying(100) COLLATE 
+    endereco_nome character varying(72) ,
+    cep character varying(8) ,
+    complemento character varying(100) 
 )
 
 TABLESPACE pg_default;
@@ -246,7 +246,7 @@ ALTER TABLE IF EXISTS endereco
 
 CREATE TABLE IF NOT EXISTS forma_pagamento
 (
-    descricao character varying(255) COLLATE  NOT NULL,
+    descricao character varying(255)  NOT NULL,
     ativo integer
 )
 
@@ -264,7 +264,7 @@ ALTER TABLE IF EXISTS forma_pagamento
 CREATE TABLE IF NOT EXISTS unidade
 (
     id serial PRIMARY KEY,
-    unidade character varying(6) COLLATE  NOT NULL
+    unidade character varying(6)  NOT NULL
     
 )
 
